f40a05666ebf4f1c1f2b14dbc2df63551348129f
Ilya Semenov <semenov@inetss.com>
2023-04-18 14:50:46 +0700

    test: add basic test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,61 @@
+import { Model } from "objection"
 import reorder from "objection-reorder"
-import { expect, test } from "vitest"
+import { afterEach, beforeEach, expect, test } from "vitest"
 
-test("import works", () => {
-	// Stub test
-	expect(reorder).toBeInstanceOf(Function)
+import { setup } from "./setup"
+
+class FooModel extends Model {
+	static tableName = "foo"
+
+	id?: number
+	name?: string
+	sort_order?: number
+}
+
+const knex = await setup()
+
+beforeEach(async () => {
+	await knex.schema.createTable("foo", (table) => {
+		table.integer("id").notNullable().primary()
+		table.string("name").notNullable()
+		table.integer("sort_order").notNullable()
+	})
+})
+
+afterEach(async () => {
+	await knex.schema.dropTable("foo")
+})
+
+async function expect_order(names: string[]) {
+	expect(
+		await FooModel.query()
+			.select("name")
+			.orderBy("sort_order")
+			.then((foos) => foos.map((foo) => foo.name))
+	).toEqual(names)
+}
+
+test("access fields", async () => {
+	await FooModel.query().insertGraph([
+		{ id: 10, name: "Foo", sort_order: 0 },
+		{ id: 3, name: "Bar", sort_order: 1 },
+		{ id: 8, name: "Baz", sort_order: 2 },
+		{ id: 7, name: "Qoox", sort_order: 3 },
+	])
+
+	// put Foo onto position 2, between Baz and Qoox
+	await reorder(FooModel.query(), 10, 2)
+	await expect_order(["Bar", "Baz", "Foo", "Qoox"])
+
+	// put Foo onto position 0
+	await reorder(FooModel.query(), 10, 0)
+	await expect_order(["Foo", "Bar", "Baz", "Qoox"])
+
+	// put Foo onto position 3
+	await reorder(FooModel.query(), 10, 3)
+	await expect_order(["Bar", "Baz", "Qoox", "Foo"])
+
+	// put Baz onto position 3
+	await reorder(FooModel.query(), 8, 3)
+	await expect_order(["Bar", "Qoox", "Foo", "Baz"])
 })
---
+++ tests/setup.ts
@@ -0,0 +1,14 @@
+import Knex from "knex"
+import { Model } from "objection"
+import { afterAll } from "vitest"
+
+export async function setup() {
+	const knex = Knex({
+		client: "sqlite3",
+		connection: ":memory:",
+		useNullAsDefault: true,
+	})
+	afterAll(() => knex.destroy())
+	Model.knex(knex)
+	return knex
+}
